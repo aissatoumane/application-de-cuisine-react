87d5d2af7ad9b4fadf0aa520bd0155689844e020
MANEA <aissatoumane17@gmail.com>
2022-05-11 16:24:00 +0200

    récupération des données de l'API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
+import Recipes from './components/Recipes';
 
 const App = () => {
   return (
     <div>
       <h1> La cuisine de Mané </h1>
+      <Recipes />
     </div>
   );
 };
 
 export default App;
+
+/* /!\ BIEN PENSER à remettre un c minuscule sur le nom de la route car j'ai mis un C majuscule quand j'ai créé le dossier components */
